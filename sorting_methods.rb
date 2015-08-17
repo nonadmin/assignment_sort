32f934e3b81eb0ed15802232b05ed2686b3f2cfa
Justin Mullis <justin.mullis@gmail.com>
2015-08-16 21:03:47 -0400

    merge sort

--- sorting_methods.rb
+++ sorting_methods.rb
@@ -1,14 +1,15 @@
+require 'pry-byebug'
+
 class SortingMethods
 
   def insertion_sort(array)
-
     steps = 0
     sorted_index = 0
-    compare_index = 1
+    comparing_index = 1
 
-    while compare_index < array.length
+    while comparing_index < array.length
 
-      compare_value = array[compare_index]
+      compare_value = array[comparing_index]
 
       while (sorted_index >= 0 && compare_value < array[sorted_index])
         array[sorted_index + 1] = array[sorted_index]
@@ -19,19 +20,17 @@ class SortingMethods
       steps += 1
       array[sorted_index + 1] = compare_value
 
-      sorted_index = compare_index
-      compare_index += 1
+      sorted_index = comparing_index
+      comparing_index += 1
 
     end
 
     puts "Steps: #{steps}"
     print array
-
   end
 
 
   def bubble_sort(array)
-
     steps = 0
 
     loop do
@@ -47,7 +46,6 @@ class SortingMethods
           array[a_index] = array[b_index]
           array[b_index] = temp
           swaps += 1
-          steps += 1
         end
 
         a_index += 1
@@ -62,9 +60,46 @@ class SortingMethods
 
     puts "Steps: #{steps}"
     print array
-
   end
 
-  
-  
+
+  def merge_sort(array)
+
+    if array.length == 1
+      
+      return array
+
+    else
+
+      right = array
+      slice_point = array.length / 2 - 1
+      left = right.slice!(0..slice_point)
+
+      right = merge_sort(right)
+      left = merge_sort(left)
+
+      sorted_array = []
+
+      until right.empty? && left.empty?
+        
+        if right[0].nil?
+          sorted_array << left.shift
+        elsif left[0].nil?
+          sorted_array << right.shift
+        elsif right[0] == left[0]
+          sorted_array << right.shift
+          sorted_array << left.shift
+        else
+          sorted_value = right[0] < left[0] ? right.shift : left.shift
+          sorted_array << sorted_value
+        end
+
+      end
+
+    end
+
+    sorted_array
+    
+  end
+
 end
